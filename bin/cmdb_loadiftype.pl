390469f3f3eea15333e6e76d235f6415acd0adfd
theob <theob@192.168.178.20>
2013-06-09 10:17:07 +0200

    Added copyright notice

--- bin/cmdb_loadiftype.pl
+++ bin/cmdb_loadiftype.pl
@@ -1,7 +1,44 @@
 #!/usr/bin/env perl
 
+=begin nd
+
+    Script: cmdb_loadiftype.pl
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2012-2013 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    Create conversions from IANA iftype files
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+	
+
+=cut
+
+use v5.16.0;
 use Logger;
 use ActiveCMDB::Common::Conversion;
+use strict;
 
 my $file = sprintf("%s/var/mibs/IANA/ianaiftype-mib", $ENV{CMDB_HOME});
 open(FH, "<", $file) or die "Unable to open mib file";
@@ -11,10 +48,12 @@ while ( <FH> )
 	if ( /IANAifType ::= TEXTUAL-CONVENTION/ ) { $in++ }
 	if ( $in && /\{/ ) { $in++ }
 	if ( $in == 2 && /(.+)\((\d+)\)/ ) {
-		$n = $1;
-		$v = $2;
-		$n =~ s/^\s+//;
+		my $n = $1;
+		my $v = $2;
+		my $n =~ s/^\s+//;
 		cmdb_add_conversion('ifType', $v, $n);
 	}
 	if ( $in == 2 && /\}/ ) { $in = 0; }
 }
+
+exit;
