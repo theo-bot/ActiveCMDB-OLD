1ea4e8327504a42266fac061cd55cb5554736e11
theob <theob@192.168.178.20>
2013-06-09 11:02:05 +0200

    Added copyright notice

--- lib/ActiveCMDB/ConfigFactory.pm
+++ lib/ActiveCMDB/ConfigFactory.pm
@@ -1,5 +1,39 @@
 package ActiveCMDB::ConfigFactory;
 
+=begin nd
+
+    Script: ActiveCMDB::ConfigFactory
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2012-2013 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    Configuration System Library
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+
+=cut
+
 use base qw( Class::Singleton );
 use Config::JFDI;
 use File::stat;
--- lib/ActiveCMDB/Schema.pm
+++ lib/ActiveCMDB/Schema.pm
@@ -14,6 +14,39 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07025 @ 2012-08-21 09:05:29
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:VFXcqkMRpF3Bn9jjTjMYPw
 
+=begin nd
+
+    Script: ActiveCMDB::Schema
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2012-2013 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    Schema
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+
+=cut
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
