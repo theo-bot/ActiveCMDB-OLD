5fe33a8c744b1a61d4124bf5e4794937d18150cc
theob <thg.bot@gmail.com>
2013-06-10 11:14:06 +0200

    Added copyright notice

--- lib/ActiveCMDB/Object/IpType.pm
+++ lib/ActiveCMDB/Object/IpType.pm
@@ -1,8 +1,42 @@
 package ActiveCMDB::Object::IpType;
 
-#
-# Include required modules 
-#
+=begin nd
+
+    Script: ActiveCMDB::Object::IpType.pm
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2012-2013 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    Object class definition for IP Device Types
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+
+	
+=cut
+
+#########################################################################
+# Initialize  modules
 use Moose;
 use Moose::Util::TypeConstraints;
 use Try::Tiny;
