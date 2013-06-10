cef9d5e1b432691e30deb1364e336c306bacd694
theob <thg.bot@gmail.com>
2013-06-10 15:09:43 +0200

    Added copyright notice

--- lib/Logger.pm
+++ lib/Logger.pm
@@ -1,5 +1,40 @@
 package Logger;
 
+=begin nd
+
+    Script: Logger.pm
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2012-2013 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    Global logging library
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+
+	
+=cut
+
 use Log::Log4perl;
 use base 'Class::Singleton';
 use ActiveCMDB::Common;
