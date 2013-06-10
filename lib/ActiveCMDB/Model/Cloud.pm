f95c0c4bb58435b2fcfaa28d004dd0adbd055d53
theob <thg.bot@gmail.com>
2013-06-10 10:57:29 +0200

    Added copyright notice

--- lib/ActiveCMDB/Model/Cloud.pm
+++ lib/ActiveCMDB/Model/Cloud.pm
@@ -1,5 +1,42 @@
 package ActiveCMDB::Model::Cloud;
 
+=begin nd
+
+    Script: ActiveCMDB::Model::Cloud.pm
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2012-2013 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    Catalyst Model for Catalyst::Model::Riak
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+
+	
+=cut
+
+#########################################################################
+# Initialize  modules
 use Moose;
 use ActiveCMDB::ConfigFactory;
 my $config = ActiveCMDB::ConfigFactory->instance();
