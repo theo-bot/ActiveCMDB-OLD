ef469f4393bb9135af6010ef69e7f80ad77e69dc
theob <thg.bot@gmail.com>
2013-06-10 14:50:04 +0200

    Added copyright notice

--- lib/ActiveCMDB/View/JSON.pm
+++ lib/ActiveCMDB/View/JSON.pm
@@ -1,29 +1,44 @@
 package ActiveCMDB::View::JSON;
 
+=begin nd
+
+    Script: ActiveCMDB::View::JSON.pm
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2012-2013 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    Catalyst View for JSON exposure
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+
+	
+=cut
+
+
 use strict;
 use base 'Catalyst::View::JSON';
 
-=head1 NAME
 
-ActiveCMDB::View::JSON - Catalyst JSON View
-
-=head1 SYNOPSIS
-
-See L<ActiveCMDB>
-
-=head1 DESCRIPTION
-
-Catalyst JSON View.
-
-=head1 AUTHOR
-
-Theo Bot
-
-=head1 LICENSE
-
-This library is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
