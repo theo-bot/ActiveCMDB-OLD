4c4943a27de6889a42edd9bc089c8e50784f3b22
theob <thg.bot@gmail.com>
2013-06-12 08:46:57 +0200

    Added copyright notice

--- root/static/js/activecmdb.structure.js
+++ root/static/js/activecmdb.structure.js
@@ -1,3 +1,32 @@
+/*
+ * 	Script:	activecmdb.config.js
+ * 	___________________________________________________________
+ *
+ * 	Copyright (C) 2011-2015 Theo Bot
+ *
+ * 	http://www.activecmdb.org
+ *
+ * 	Topic: Purpose
+ *
+ * 	JS Function(s) to support dynamic (ajax) device entities
+ *
+ * 	About: License
+ *
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ *  Topic: Version Control
+ *
+ *
+ */
+
 function xEntityDetails(device_id,index)
 {
 	$.ajax({
