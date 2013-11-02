1a8c9f0619a4f1c432ecf89d5737c5cc647fa23c
theob <theob@192.168.1.20>
2013-11-02 11:04:51 +0100

    Initial import

---
+++ bin/activecmdb.sh
@@ -0,0 +1,11 @@
+
+
+CMDB_HOME=<your installation directory>
+
+# DO NOT EDIT BELOW THIS LINE
+PATH=$CMDB_HOME/platform/Linux/x86_64/bin:$PATH
+PERL5LIB=$CMDB_HOME/lib
+DBIC_DT_SEARCH_OK=true
+
+
+export PERL5LIB CMDB_HOME
