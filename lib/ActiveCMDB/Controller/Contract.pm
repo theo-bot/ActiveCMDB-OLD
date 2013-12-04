81059d8e3eabf9da854044acd06cf2aa242404ef
theob <theob@192.168.1.20>
2013-12-04 15:01:42 +0100

    Fixed issue #245510

--- lib/ActiveCMDB/Controller/Contract.pm
+++ lib/ActiveCMDB/Controller/Contract.pm
@@ -55,145 +55,176 @@ my $strp = DateTime::Format::Strptime->new(
 sub index :Private {
     my ( $self, $c ) = @_;
 	
-	my $format = $config->section('cmdb::default::date_format');
-	$format =~ s/\%Y/yy/;
-	$format =~ s/\%m/mm/;
-	$format =~ s/\d%/dd/;
+	if ( $c->check_user_roles('contractAdmin') || $c->check_user_roles('contractViewer') )
+	{
+		my $format = $config->section('cmdb::default::date_format');
+		$format =~ s/\%Y/yy/;
+		$format =~ s/\%m/mm/;
+		$format =~ s/\d%/dd/;
 	
-	$format =~ s/\%//g;
-	
-	$c->stash->{dateFormat} = $format;
-	$c->stash->{template} = 'contract/container.tt';
-   
+		$format =~ s/\%//g;
+		
+		$c->stash->{dateFormat} = $format;
+		$c->stash->{template} = 'contract/container.tt';
+	} else {
+		$c->response->redirect($c->uri_for($c->controller('Root')->action_for('noauth')));
+	}
 }
 
 sub api :Local {
 	my($self, $c) = @_;
 	
-	if ( defined($c->request->params->{oper}) ) {
-		$c->forward('/contract/' . $c->request->params->{oper});
+	if ( $c->check_user_roles('contractAdmin') || $c->check_user_roles('contractViewer') )
+	{
+		if ( defined($c->request->params->{oper}) ) {
+			$c->forward('/contract/' . $c->request->params->{oper});
+		}
+	} else {
+		$c->response->redirect($c->uri_for($c->controller('Root')->action_for('noauth')));
 	}
 }
 
 sub list :Local {
 	my($self, $c) = @_;
-	my($rs, $json, $search);
-	my @rows = ();
 	
-	my $rows	= $c->request->params->{rows} || 10;
-	my $page	= $c->request->params->{page} || 1;
-	my $order	= $c->request->params->{sidx} || 'contract_number';
-	my $asc		= '-' . $c->request->params->{sord};
-	
-	#
-	# Create search filter
-	#
-	if ( $c->request->params->{_search} eq 'true' )
+	if ( $c->check_user_roles('contractAdmin') || $c->check_user_roles('contractViewer') )
 	{
-		my $searchOper   = $c->request->params->{searchOper};
-		my $searchField  = $c->request->params->{searchField};
-		my $searchString = $c->request->params->{searchString};
+		my($rs, $json, $search);
+		my @rows = ();
+	
+		my $rows	= $c->request->params->{rows} || 10;
+		my $page	= $c->request->params->{page} || 1;
+		my $order	= $c->request->params->{sidx} || 'contract_number';
+		my $asc		= '-' . $c->request->params->{sord};
+	
+		#
+		# Create search filter
+		#
+		if ( $c->request->params->{_search} eq 'true' )
+		{
+			my $searchOper   = $c->request->params->{searchOper};
+			my $searchField  = $c->request->params->{searchField};
+			my $searchString = $c->request->params->{searchString};
 		
-		switch ( $searchOper ) {
-			case 'cn'		{ $search = { $searchField => { like => '%'.$searchString.'%' } } }
-			case 'eq'		{ $search = { $searchField => $searchString } }
-			case 'ne'		{ $search = { $searchField => { '!=' => $searchString } } }
-			case 'bw'		{ $search = { $searchField => { like => $searchString.'%' } } }
-			else 			{ $search = { } }
+			switch ( $searchOper ) {
+				case 'cn'		{ $search = { $searchField => { like => '%'.$searchString.'%' } } }
+				case 'eq'		{ $search = { $searchField => $searchString } }
+				case 'ne'		{ $search = { $searchField => { '!=' => $searchString } } }
+				case 'bw'		{ $search = { $searchField => { like => $searchString.'%' } } }
+				else 			{ $search = { } }
+			}
+		} else {
+			$search = { };
 		}
-	} else {
-		$search = { };
-	}
-	$c->log->debug(Dumper($search));
+		$c->log->debug(Dumper($search));
 	
-	$rs = $c->model("CMDBv1::Contract")->search(
-				$search,
-				{
-					rows		=> $rows,
-					page		=> $page,
-					join		=> 'vendor',
-					'+select'	=> ['vendor.vendor_name'],
-					'+as'		=> ['vendor_name'],
-					order_by	=> { $asc => $order },
-				}
-	);
-	
-	$json->{records} = $rs->count;
-	if ( $json->{records} > 0 ) {
-		$json->{total} = ceil($json->{records} / $c->request->params->{rows} );
-	} else {
-		$json->{total} = 0;
-	} 
-	
-	while ( my $row = $rs->next )
-	{
-		my $start = substr($row->start_date,0,10);
-		my $end   = substr($row->end_date,0,10);
-		push(@rows, { id => $row->contract_id, cell=> [
-														$row->contract_number,
-														$row->contract_descr,
-														$row->get_column('vendor_name'),
-														$start,
-														$end
-													]
+		$rs = $c->model("CMDBv1::Contract")->search(
+					$search,
+					{
+						rows		=> $rows,
+						page		=> $page,
+						join		=> 'vendor',
+						'+select'	=> ['vendor.vendor_name'],
+						'+as'		=> ['vendor_name'],
+						order_by	=> { $asc => $order },
 					}
-			);
-	}
+		);
 	
-	$json->{rows} = [ @rows ];
-	$c->stash->{json} = $json;
-	$c->forward( $c->view('JSON') );
+		$json->{records} = $rs->count;
+		if ( $json->{records} > 0 ) {
+			$json->{total} = ceil($json->{records} / $c->request->params->{rows} );
+		} else {
+			$json->{total} = 0;
+		} 
+	
+		while ( my $row = $rs->next )
+		{
+			my $start = substr($row->start_date,0,10);
+			my $end   = substr($row->end_date,0,10);
+			push(@rows, { id => $row->contract_id, cell=> [
+															$row->contract_number,
+															$row->contract_descr,
+															$row->get_column('vendor_name'),
+															$start,
+															$end
+														]
+						}
+				);
+		}
+	
+		$json->{rows} = [ @rows ];
+		$c->stash->{json} = $json;
+		$c->forward( $c->view('JSON') );
+	} else {
+		$c->response->redirect($c->uri_for($c->controller('Root')->action_for('noauth')));
+	}
 }
 
 sub edit :Local {
 	my($self, $c) = @_;
 	
-	if ( defined($c->request->params->{id}) )
+	if ( $c->check_user_roles('contractAdmin') )
 	{
-		$c->log->debug("Fetching data for contract id " . $c->request->params->{id} );
-		my $contract = ActiveCMDB::Object::Contract->new(id => $c->request->params->{id} );
-		$contract->get_data();
-		$c->stash->{contract} = $contract;
-		$c->log->info("Found data for contract number " . $contract->number);
-		$c->log->debug("Contract vendor_id " . $contract->vendor_id);
+		if ( defined($c->request->params->{id}) )
+		{
+			$c->log->debug("Fetching data for contract id " . $c->request->params->{id} );
+			my $contract = ActiveCMDB::Object::Contract->new(id => $c->request->params->{id} );
+			$contract->get_data();
+			$c->stash->{contract} = $contract;
+			$c->log->info("Found data for contract number " . $contract->number);
+			$c->log->debug("Contract vendor_id " . $contract->vendor_id);
+		} else {
+			$c->log->warn("Contract id was not defined or zero");
+		}
+		my %vendors = cmdb_get_vendors();
+		$c->stash->{vendors} = \%vendors;
+		$c->stash->{template} = 'contract/edit.tt';
 	} else {
-		$c->log->warn("Contract id was not defined or zero");
+		$c->response->redirect($c->uri_for($c->controller('Root')->action_for('noauth')));
 	}
-	my %vendors = cmdb_get_vendors();
-	$c->stash->{vendors} = \%vendors;
-	$c->stash->{template} = 'contract/edit.tt';
 }
 
 sub add :Local {
 	my($self, $c) = @_;
 	
-	my $contract = ActiveCMDB::Object::Contract->new();
+	if ( $c->check_user_roles('contractAdmin') )
+	{
+		my $contract = ActiveCMDB::Object::Contract->new();
 	
-	$contract->service_hours('540;1020');
-	my %vendors = cmdb_get_vendors();
+		$contract->service_hours('540;1020');
+		my %vendors = cmdb_get_vendors();
 	
-	$c->stash->{contract} = $contract;
-	$c->stash->{vendors} = \%vendors;
-	$c->stash->{template} = 'contract/edit.tt';
+		$c->stash->{contract} = $contract;
+		$c->stash->{vendors} = \%vendors;
+		$c->stash->{template} = 'contract/edit.tt';
+	} else {
+		$c->response->redirect($c->uri_for($c->controller('Root')->action_for('noauth')));
+	}
 }
 
 sub save :Local {
 	my($self, $c) = @_;
 	
-	if ( defined($c->request->params->{id}) )
+	if ( $c->check_user_roles('contractAdmin') )
 	{
-		$c->log->info("Saving contract data");
-		my $id = int($c->request->params->{id});
-		my $contract = ActiveCMDB::Object::Contract->new( id => $id );
-		$contract->populate($c->request->params);
-		$contract->save();
+		if ( defined($c->request->params->{id}) )
+		{
+			$c->log->info("Saving contract data");
+			my $id = int($c->request->params->{id});
+			my $contract = ActiveCMDB::Object::Contract->new( id => $id );
+			$contract->populate($c->request->params);
+			$contract->save();
 		
-	} else {
-		$c->log->warn("Unable to save contract id not found.");
-	}
+		} else {
+			$c->log->warn("Unable to save contract id not found.");
+		}
 	
-	$c->response->body('Done');
-	$c->response->status(200);
+		$c->response->body('Done');
+		$c->response->status(200);
+	} else {
+		$c->response->body('Failed');
+		$c->response->status(401);
+	}
 }
 
 sub vendor_data :Local {
