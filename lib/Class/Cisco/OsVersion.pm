8d51338ba0d5e510ef2926aa9ec13062917f9a1d
theob <theob@192.168.1.20>
2013-12-20 07:28:24 +0100

    Added copyright header

--- lib/Class/Cisco/OsVersion.pm
+++ lib/Class/Cisco/OsVersion.pm
@@ -1,4 +1,44 @@
 package Class::Cisco::OsVersion;
+
+=begin nd
+
+    Script: Class::Cisco::OsVersion.pm
+    ___________________________________________________________________________
+
+    Version 1.0
+
+    Copyright (C) 2011-2015 Theo Bot
+
+    http://www.activecmdb.org
+
+
+    Topic: Purpose
+
+    OS Version Helper class for cisco devices
+
+    About: License
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Topic: Release information
+
+    $Rev$
+
+	Topic: Description
+	
+	This a helper to discover the os version on cisco operations
+	
+	
+=cut
+
 use v5.16.0;
 use Moose::Role;
 
